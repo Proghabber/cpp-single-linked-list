55acbcbcb8bd06996e38198c8c8b3686aab36e3c
Alex <rozenrol@mail.ru>
2024-02-04 17:05:29 +0800

    first version

--- string_processing.h
+++
@@ -1,6 +0,0 @@
-//Вставьте сюда своё решение из урока «Очередь запросов» темы «Стек, очередь, дек».
-#pragma once
-#include <vector>
-#include <string>
-
-std::vector<std::string> SplitIntoWords(const std::string& text);
